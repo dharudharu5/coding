6afbb3ce5eb98f15ca0b98a6add8abca229f520b
dharudharu5 <mail4dhaarani@gmail.com>
2024-07-12 13:51:36 +0530

    Update hashMap.js

--- GlobalTrend_Programming_Assessment/hashMap.js
+++ GlobalTrend_Programming_Assessment/hashMap.js
@@ -3,29 +3,25 @@ class HashMap {
         this.map = {};
     }
 
-    // Method to add a key-value pair to the HashMap
-    put(key, value) {
+      put(key, value) {
         this.map[key] = value;
     }
 
-    // Method to retrieve the value for a given key
-    get(key) {
+     get(key) {
         return this.map.hasOwnProperty(key) ? this.map[key] : undefined;
     }
 
-    // Method to remove a key-value pair from the HashMap
-    remove(key) {
+       remove(key) {
         if (this.map.hasOwnProperty(key)) {
             delete this.map[key];
         }
     }
 }
 
-// Example usage:
 const hashMap = new HashMap();
 hashMap.put('name', 'Alice');
-console.log(hashMap.get('name')); // Outputs: Alice
+console.log(hashMap.get('name'));
 hashMap.put('age', 25);
-console.log(hashMap.get('age')); // Outputs: 25
+console.log(hashMap.get('age'));
 hashMap.remove('name');
-console.log(hashMap.get('name')); // Outputs: undefined
+console.log(hashMap.get('name')); 
