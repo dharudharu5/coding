413704efdc93db1c693c18e32088f54e3fa91f90
dharudharu5 <mail4dhaarani@gmail.com>
2024-07-12 13:49:05 +0530

    Update evenNumber.js

--- GlobalTrend_Programming_Assessment/evenNumber.js
+++ GlobalTrend_Programming_Assessment/evenNumber.js
@@ -2,8 +2,7 @@ function filterEvenNumbers(array) {
     return array.filter(number => number % 2 !== 0);
 }
 
-// Example usage:
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); // Outputs: [1, 3, 5]
-console.log(filterEvenNumbers([12, 15, 18, 21, 24])); // Outputs: [15, 21]
-console.log(filterEvenNumbers([2, 4, 6, 8, 10])); // Outputs: []
-console.log(filterEvenNumbers([1, 3, 5, 7, 9])); // Outputs: [1, 3, 5, 7, 9]
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); 
+console.log(filterEvenNumbers([12, 15, 18, 21, 24]));
+console.log(filterEvenNumbers([2, 4, 6, 8, 10])); 
+console.log(filterEvenNumbers([1, 3, 5, 7, 9])); 
