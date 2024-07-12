7f6792011f7558efff819cb1ee46e8002d62ee69
dharudharu5 <mail4dhaarani@gmail.com>
2024-07-12 13:52:17 +0530

    Update removeDuplicate.js

--- GlobalTrend_Programming_Assessment/removeDuplicate.js
+++ GlobalTrend_Programming_Assessment/removeDuplicate.js
@@ -1,14 +1,11 @@
 function removeDuplicates(array) {
-    // Use a Set to remove duplicates, since Sets only allow unique values
-    let uniqueSet = new Set(array);
+       let uniqueSet = new Set(array);
     
-    // Convert the Set back to an array
-    let uniqueArray = Array.from(uniqueSet);
+       let uniqueArray = Array.from(uniqueSet);
     
     return uniqueArray;
 }
 
-// Example usage:
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Outputs: [1, 2, 3, 4, 5]
-console.log(removeDuplicates(['a', 'b', 'a', 'c', 'b'])); // Outputs: ['a', 'b', 'c']
-console.log(removeDuplicates([1, '1', 2, '2', 1, '1'])); // Outputs: [1, '1', 2, '2']
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+console.log(removeDuplicates(['a', 'b', 'a', 'c', 'b']));
+console.log(removeDuplicates([1, '1', 2, '2', 1, '1']));
